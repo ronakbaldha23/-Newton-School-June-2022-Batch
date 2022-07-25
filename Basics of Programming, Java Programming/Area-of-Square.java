31690a6f104b5defcb94baa249f85e927771e1bb
Ronak Baldha <86797379+ronakbaldha23@users.noreply.github.com>
2022-07-25 23:22:18 +0530

    Create Area-of-Square.java

---
+++ Basics of Programming, Java Programming/Area-of-Square.java
@@ -0,0 +1,18 @@
+/* Author = Ronak Baldha */
+/* GitHub Id = ronakbaldha23 */
+/* Language = JAVA */
+/* Address = Gujarat,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println((n*n));
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
