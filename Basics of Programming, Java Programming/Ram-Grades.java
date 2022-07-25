74721c27f114dd9b05330cc917cb263b330becaa
Ronak Baldha <86797379+ronakbaldha23@users.noreply.github.com>
2022-07-25 23:24:06 +0530

    Create Ram-Grades.java

---
+++ Basics of Programming, Java Programming/Ram-Grades.java
@@ -0,0 +1,21 @@
+/* Author = Ronak Baldha */
+/* GitHub Id = ronakbaldha23 */
+/* Language = JAVA */
+/* Address = Gujarat,INDIA */
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+		System.out.println(((a+b+c+d)/4));
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
